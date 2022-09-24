1a3b670fba0910653522150840e74f07bfbeab3e
raymondtju <75233529+raymondtju@users.noreply.github.com>
2022-09-24 16:41:33 +0700

    2 files modified, 1 added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-const { createApp } = require('vue');
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from '@/router'
 
-createApp(App).mount("#app");
+
+createApp(App)
+.use(router)
+.mount('#app')
